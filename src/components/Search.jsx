800eec6071e68c8afc143dc3d9cfbf05439fde96
Amangoua Ange <adagou@yahoo.com>
2023-09-20 19:28:44 +0000

    search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,21 +2,34 @@ import SearchIcon from '@mui/icons-material/Search'
 import MicNoneIcon from '@mui/icons-material/MicNone'
 import './Search.css'
 import { Button } from '@mui/material'
+import { useState } from 'react'
 
 function Search() {
+  const [input, setInput] = useState('')
+
+  const search = e => {
+    e.preventDefault()
+  }
+
   return (
-    <div className='search'>
+    <form className='search'>
       <div className='search__input'>
         <SearchIcon />
-        <input type='text' />
+        <input
+          type='text'
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
         <MicNoneIcon />
       </div>
 
       <div className='search__buttons'>
-        <Button variant='outlined'>Google Search</Button>
+        <Button variant='outlined' onClick={search} type='submit'>
+          Google Search
+        </Button>
         <Button variant='outlined'>I'm Feeling Lucky </Button>
       </div>
-    </div>
+    </form>
   )
 }
 
